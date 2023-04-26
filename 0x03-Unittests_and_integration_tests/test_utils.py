2d71db9efab6c10bf256038cc70a13080c8df2b8
Jamal <jayjamal86@gmail.com>
2023-04-26 12:00:21 +0300

    parametrize tests #2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,6 +24,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map function."""
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test access_nested_map function."""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == "__main__":
     unittest.main()
