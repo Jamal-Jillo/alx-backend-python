43cf63021f63f8c13b269a0291dfecaba0cc5d43
Jamal <jayjamal86@gmail.com>
2023-04-26 18:31:32 +0300

    fixes

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -72,8 +72,7 @@ class TestMemoize(unittest.TestCase):
                 """A property."""
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) \
-                as mock_method:
+        with patch.object(TestClass, 'a_method') as mock_method:
             test = TestClass()
             test.a_property
             test.a_property
